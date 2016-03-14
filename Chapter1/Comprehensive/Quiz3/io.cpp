0956739692ef676560f43c7754531675c89adcae
ElBartoWasHere <ElBartoWasHere@users.noreply.github.com>
2016-03-14 11:44:21 -0500

    Create io.cpp

---
+++ Chapter1/Comprehensive/Quiz3/io.cpp
@@ -0,0 +1,12 @@
+int readNumber()
+{
+  std::cout << "Please enter number: " << std::endl;
+  int temp;
+  std::cin >> temp;
+  retutn temp;
+}
+
+void writeAnswer(int x)
+{
+  std::cout << "Numbers added: " << x << std::endl;
+}
